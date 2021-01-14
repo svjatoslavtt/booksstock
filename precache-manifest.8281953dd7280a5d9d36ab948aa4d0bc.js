6f5c44eee66258843249ffb92d66e76ce3211200
svyatoslav <svyatoslavtt>
2021-01-14 21:01:39 +0200

    Updates

---
+++ precache-manifest.8281953dd7280a5d9d36ab948aa4d0bc.js
@@ -0,0 +1,242 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c1f237183730439bb1e1c418712b129",
+    "url": "/booksstock/index.html"
+  },
+  {
+    "revision": "f897c519dc051e63a605",
+    "url": "/booksstock/static/css/2.de26ae72.chunk.css"
+  },
+  {
+    "revision": "b5df0517e53159175eae",
+    "url": "/booksstock/static/css/main.f9f26eeb.chunk.css"
+  },
+  {
+    "revision": "f897c519dc051e63a605",
+    "url": "/booksstock/static/js/2.4e8640a8.chunk.js"
+  },
+  {
+    "revision": "e3cddcc14d0b349b4e6eccba623e55ae",
+    "url": "/booksstock/static/js/2.4e8640a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b5df0517e53159175eae",
+    "url": "/booksstock/static/js/main.4478f247.chunk.js"
+  },
+  {
+    "revision": "5a895d9bfba105b0a8af",
+    "url": "/booksstock/static/js/runtime-main.e1955f89.js"
+  },
+  {
+    "revision": "e07df86cef2e721115583d61d1fb68a6",
+    "url": "/booksstock/static/media/Roboto-Bold.e07df86c.ttf"
+  },
+  {
+    "revision": "88823c2015ffd5fa89d567e17297a137",
+    "url": "/booksstock/static/media/Roboto-Light.88823c20.ttf"
+  },
+  {
+    "revision": "a3ce4440f2abf76f4a1b14b83920138c",
+    "url": "/booksstock/static/media/Roboto-LightItalic.a3ce4440.ttf"
+  },
+  {
+    "revision": "11eabca2251325cfc5589c9c6fb57b46",
+    "url": "/booksstock/static/media/Roboto-Regular.11eabca2.ttf"
+  },
+  {
+    "revision": "c64fcb0e683b40b91ec2b75b0cf41034",
+    "url": "/booksstock/static/media/announcement.c64fcb0e.webp"
+  },
+  {
+    "revision": "bed967fa55728b37b7ae7444caa021e4",
+    "url": "/booksstock/static/media/book-image1.bed967fa.webp"
+  },
+  {
+    "revision": "51e979bd3e5a2665203509eac7e0fc7b",
+    "url": "/booksstock/static/media/book-image2.51e979bd.webp"
+  },
+  {
+    "revision": "c4d40b00bbdb3d6b567fd027340fabda",
+    "url": "/booksstock/static/media/book-image3.c4d40b00.webp"
+  },
+  {
+    "revision": "71c2ce9317452394bc112a73c34b487a",
+    "url": "/booksstock/static/media/book-image4.71c2ce93.webp"
+  },
+  {
+    "revision": "1e72c7cbb85618d957162c2bf32d5aba",
+    "url": "/booksstock/static/media/book-image5.1e72c7cb.webp"
+  },
+  {
+    "revision": "13b621c6b3722cd1a9ccaca4bca96c8e",
+    "url": "/booksstock/static/media/genre1.13b621c6.jpg"
+  },
+  {
+    "revision": "8ea2138c61bbde9cb262891979c3510b",
+    "url": "/booksstock/static/media/genre10.8ea2138c.jpg"
+  },
+  {
+    "revision": "97882e321eb73e2b5929fae91c97ca9e",
+    "url": "/booksstock/static/media/genre11.97882e32.jpg"
+  },
+  {
+    "revision": "205a16ddd96f2676ad52d048ffb1f3c6",
+    "url": "/booksstock/static/media/genre12.205a16dd.jpg"
+  },
+  {
+    "revision": "f75e37e7b18413b4ae020c3c7168aa87",
+    "url": "/booksstock/static/media/genre13.f75e37e7.jpg"
+  },
+  {
+    "revision": "639c2922f1cbd22f0a9111b7769d9aaa",
+    "url": "/booksstock/static/media/genre14.639c2922.jpg"
+  },
+  {
+    "revision": "1918efaadb153a0e910de159f4d22198",
+    "url": "/booksstock/static/media/genre15.1918efaa.jpg"
+  },
+  {
+    "revision": "95dd2d7cf12346a04e7c1301e66c52c3",
+    "url": "/booksstock/static/media/genre16.95dd2d7c.jpg"
+  },
+  {
+    "revision": "aed82fc9161e55c86bd9ef8d3f9e4c82",
+    "url": "/booksstock/static/media/genre17.aed82fc9.jpg"
+  },
+  {
+    "revision": "ce1d3f153c5026f385c994b76bee0715",
+    "url": "/booksstock/static/media/genre18.ce1d3f15.jpg"
+  },
+  {
+    "revision": "7aac4ef7aa98f2adc47f8aa43b1b114c",
+    "url": "/booksstock/static/media/genre19.7aac4ef7.jpg"
+  },
+  {
+    "revision": "fa24a00d08ffcc2c25dc4165ef196460",
+    "url": "/booksstock/static/media/genre1_big.fa24a00d.jpg"
+  },
+  {
+    "revision": "6cc0570a0037f3dc511f31bf3e2e5f1c",
+    "url": "/booksstock/static/media/genre2.6cc0570a.jpg"
+  },
+  {
+    "revision": "d5564430ecb399f3f7193d21eb5c1e98",
+    "url": "/booksstock/static/media/genre20.d5564430.jpg"
+  },
+  {
+    "revision": "5a6de275cda6ff29f6ad7dc29bab6a4c",
+    "url": "/booksstock/static/media/genre21.5a6de275.jpg"
+  },
+  {
+    "revision": "5ec0a4da0634b17d731fb4fd1ecd9b4b",
+    "url": "/booksstock/static/media/genre22.5ec0a4da.jpg"
+  },
+  {
+    "revision": "404d29a5fb08c8d87d52445794552e84",
+    "url": "/booksstock/static/media/genre23.404d29a5.jpg"
+  },
+  {
+    "revision": "f604886cef2a17cbd1ee0bd9c39370ab",
+    "url": "/booksstock/static/media/genre24.f604886c.jpg"
+  },
+  {
+    "revision": "145db7ddb946bd96d853986ae10a5d13",
+    "url": "/booksstock/static/media/genre25.145db7dd.jpg"
+  },
+  {
+    "revision": "1afa16e86ca5c46c889afbebaa03edae",
+    "url": "/booksstock/static/media/genre26.1afa16e8.jpg"
+  },
+  {
+    "revision": "8f45fb51d16be9cd68ae0992d2dd13c0",
+    "url": "/booksstock/static/media/genre27.8f45fb51.jpg"
+  },
+  {
+    "revision": "7380ee25b72541e30cc518878205e30a",
+    "url": "/booksstock/static/media/genre28.7380ee25.jpg"
+  },
+  {
+    "revision": "890370151f1f62e4cf6b3f7d9eb1d924",
+    "url": "/booksstock/static/media/genre29.89037015.jpg"
+  },
+  {
+    "revision": "dcf7d586642c4e26bbbdbdfd019f11cd",
+    "url": "/booksstock/static/media/genre3.dcf7d586.jpg"
+  },
+  {
+    "revision": "1191f689e3d92145b4b3186fddbb0a18",
+    "url": "/booksstock/static/media/genre30.1191f689.jpg"
+  },
+  {
+    "revision": "e79991c2e474473491210ac0b2fff65d",
+    "url": "/booksstock/static/media/genre4.e79991c2.jpg"
+  },
+  {
+    "revision": "d011caf626cff5aa0d92b3799df9791a",
+    "url": "/booksstock/static/media/genre5.d011caf6.jpg"
+  },
+  {
+    "revision": "7cd841a4a55e78b65a04d1d75ec69913",
+    "url": "/booksstock/static/media/genre6.7cd841a4.jpg"
+  },
+  {
+    "revision": "0a981bedea45c6bd0b7702a2ea334a00",
+    "url": "/booksstock/static/media/genre7.0a981bed.jpg"
+  },
+  {
+    "revision": "16515b48323562963f13df5ec586ef7b",
+    "url": "/booksstock/static/media/genre8.16515b48.jpg"
+  },
+  {
+    "revision": "8857960e822e459018ebdc4f99251306",
+    "url": "/booksstock/static/media/genre9.8857960e.jpg"
+  },
+  {
+    "revision": "f64899fa4d999e8b3c69cce76de56d59",
+    "url": "/booksstock/static/media/image.f64899fa.jpg"
+  },
+  {
+    "revision": "ee92b49dca0b7d8537fb50a8d47d488d",
+    "url": "/booksstock/static/media/image2.ee92b49d.jpg"
+  },
+  {
+    "revision": "8b6bd78c6d140aabca9d96c98c8b3f45",
+    "url": "/booksstock/static/media/image3.8b6bd78c.jpg"
+  },
+  {
+    "revision": "aa57eb8dc358fbbd13201a292b81f8c3",
+    "url": "/booksstock/static/media/image404.aa57eb8d.jpg"
+  },
+  {
+    "revision": "a770b6797b68e3f8920e473eb824bac0",
+    "url": "/booksstock/static/media/loader-big.a770b679.gif"
+  },
+  {
+    "revision": "7ee23b6d70b7f98e31312db86fe4bf4e",
+    "url": "/booksstock/static/media/popular-image.7ee23b6d.jpg"
+  },
+  {
+    "revision": "1a07de993d612370b9e4af98435c36ff",
+    "url": "/booksstock/static/media/popular-image2.1a07de99.jpg"
+  },
+  {
+    "revision": "12f0820c451bdc75f4d1ef97732bf6e8",
+    "url": "/booksstock/static/media/rw-widgets.12f0820c.woff"
+  },
+  {
+    "revision": "792dcd18baf5f544aabcad1883d673c2",
+    "url": "/booksstock/static/media/rw-widgets.792dcd18.svg"
+  },
+  {
+    "revision": "bc7c4a59f924cf037aad6e1f9edba366",
+    "url": "/booksstock/static/media/rw-widgets.bc7c4a59.eot"
+  },
+  {
+    "revision": "eceddf474df95d8d4a7e316668c3be85",
+    "url": "/booksstock/static/media/rw-widgets.eceddf47.ttf"
+  },
+  {
+    "revision": "f3d006d98a824241514517effe219036",
+    "url": "/booksstock/static/media/sign-in.f3d006d9.jpg"
+  }
+]);
